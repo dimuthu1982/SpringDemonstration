cb75ae04e64021d75732096e7da7141af566cbd1
dimuthu1982 <dimuthu1982@yahoo.com>
2015-12-15 20:46:08 +0530

    Preparing Project

---
+++ springdemo/src/main/java/org/dimuthu/test/springdemo/MainApplication.java
@@ -0,0 +1,13 @@
+package org.dimuthu.test.springdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class MainApplication 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
