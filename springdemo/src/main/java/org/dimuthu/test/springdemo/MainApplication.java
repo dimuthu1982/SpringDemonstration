56558a341890aa675903027e99a2a265006c2c8a
dimuthu1982 <dimuthu1982@yahoo.com>
2015-12-16 12:44:06 +0530

    Changed

--- springdemo/src/main/java/org/dimuthu/test/springdemo/MainApplication.java
+++ springdemo/src/main/java/org/dimuthu/test/springdemo/MainApplication.java
@@ -1,5 +1,9 @@
 package org.dimuthu.test.springdemo;
 
+import org.dimuthu.test.springdemo.beans.BeanRegusterWithAnnotation;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,34 @@ public class MainApplication
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+    	
+//    	Calling Bean Injection
+    	/*HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
+    	System.out.println("helloWorld.getMessage() : " + helloWorld.getMessage());*/
+    	
+//    	LifeCycle CallBack Bean
+/*    	
+    	context.registerShutdownHook();
+    	CallBackBeanMethods obj = (CallBackBeanMethods) context.getBean("callbackBean");
+		System.out.println("Your Message : " + obj.getMyName());*/
+    	
+    	/*Property Place Holder was register to inject the values from SpringData.properties
+    	 * So the value is printed as : My Message for Place Holder
+    	 */
+    	/*HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorldPlaceHolder");
+    	System.out.println("helloWorld.getMessage() : " + helloWorld.getMessage());
+    	*/
+    	
+//    	@Required annotated
+    	/*AnnotationCheckerBean annotationCheckerBean = (AnnotationCheckerBean) context.getBean("annotationChecker");
+    	System.out.println("helloWorld.getMessage() : " + annotationCheckerBean.getMessage());*/
+    	
+//    	Auto Wire
+    	/*AutoWireAnnotatorCheck autoWireAnnotatorCheck = (AutoWireAnnotatorCheck) context.getBean("autoWireAnnotationChecker");
+    	System.out.println("autoWireAnnotatorCheck.getMessage() : " + autoWireAnnotatorCheck.getHelloWorldInjection().getMessage());*/
+    	
+    	BeanRegusterWithAnnotation beanRegusterWithAnnotation = (BeanRegusterWithAnnotation) context.getBean("beanRegusterWithAnnotation");
+    	System.out.println("BeanRegusterWithAnnotation.getMessage() : " + beanRegusterWithAnnotation.getHelloWorld().getMessage());
     }
 }
